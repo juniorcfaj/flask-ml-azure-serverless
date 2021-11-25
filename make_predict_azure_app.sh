47bc378af05b5d8a100b8afea34f469302889dc1
juniorcfaj <46963611+juniorcfaj@users.noreply.github.com>
2021-11-25 10:07:57 -0300

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -26,4 +26,4 @@ curl -d '{
 }'\
      -H "Content-Type: application/json" \
      -X POST https://flask-ml-service.azurewebsites.net:$PORT/predict 
-     #your application name <yourappname>goes here
+     #your application name Flask ML App Service goes here
